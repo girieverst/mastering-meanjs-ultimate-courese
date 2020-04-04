2750dcfb32f93acf235d0585487cea774072ce35
Rupesh Kumar Tiwari <roopkt@gmail.com>
2020-04-04 17:47:22 -0400

    cart selector update

--- src/app/core/cart/cart-selector.ts
+++ src/app/core/cart/cart-selector.ts
@@ -1,4 +1,7 @@
+import { createSelector } from "reselect";
 import { CartState } from "./cart-state";
+
+export const getCartItems = (state: CartState) => state.cartItems;
 
 export const getCartItemsCount = (state: CartState) => {
   const cartItems = state.cartItems;
@@ -8,4 +11,10 @@ export const getCartItemsCount = (state: CartState) => {
   );
 
   return totalCartCount;
+};
+
+export const isItemAlreadyInCart = (state: CartState, productId: number) => {
+  return createSelector(getCartItems, (items) => {
+    return items.filter((item) => item.productId === productId).length > 0;
+  })(state);
 };
--- src/app/core/cart/cart-selectors.spec.ts
+++ src/app/core/cart/cart-selectors.spec.ts
@@ -1,6 +1,6 @@
-import { CartState } from "./cart-state";
 import { CartItem } from "./cart-item";
-import { getCartItemsCount } from './cart-selector';
+import { getCartItemsCount, isItemAlreadyInCart } from "./cart-selector";
+import { CartState } from "./cart-state";
 
 const given = beforeEach;
 const when = beforeEach;
@@ -17,7 +17,7 @@ describe("Cart Store Selectors", () => {
         imgUrl: "img/apple",
         itemTotal: 20,
         name: "apple",
-        price: 2
+        price: 2,
       };
       const fiveOranges: CartItem = {
         productId: 1,
@@ -25,10 +25,10 @@ describe("Cart Store Selectors", () => {
         imgUrl: "img/orange",
         itemTotal: 20,
         name: "orange",
-        price: 2
+        price: 2,
       };
       cartState = {
-        cartItems: [tenApples, fiveOranges]
+        cartItems: [tenApples, fiveOranges],
       };
     });
     when(() => {
@@ -37,5 +37,31 @@ describe("Cart Store Selectors", () => {
     then("I can see my total cart items count", () => {
       expect(result).toBe(15);
     });
+  });
+
+  it("can find cart item", () => {
+    const itemInCart: CartItem = {
+      productId: 1,
+      imgUrl: "img/apple",
+      itemTotal: 20,
+      name: "apple",
+      price: 2,
+      quantity: 10,
+    };
+    const itemInCart1: CartItem = {
+      productId: 2,
+      imgUrl: "img/orange",
+      itemTotal: 20,
+      name: "orange",
+      price: 2,
+      quantity: 5,
+    };
+    const state: CartState = {
+      cartItems: [itemInCart, itemInCart1]
+    };
+    const itemExists = isItemAlreadyInCart(state, 2);
+    expect(itemExists).toBeTruthy();
+    const itemExists1 = isItemAlreadyInCart(state,4);
+    expect(itemExists1).toBeFalsy();
   });
 });
