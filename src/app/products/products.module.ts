3e12ed94474663a1944554033641c3a69f3ada08
Rupesh Kumar Tiwari <roopkt@gmail.com>
2020-04-13 07:55:08 -0400

    finishing add to cart

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,15 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { SharedModule } from "@shared/shared.module";
+import { ProductDataService } from "../core/products/product-data.service";
+import { PmMaterialModule } from "../shared/material-module";
+import { ProductsRoutingModule } from "./products-routing.module";
+import { ProductsComponent } from "./products/products.component";
 
-import { ProductsRoutingModule } from './products-routing.module';
-import { ProductsComponent } from './products/products.component';
-import { ProductDataService } from '../core/products/product-data.service';
-import { PmMaterialModule } from '../shared/material-module';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [ProductsComponent],
-  imports: [CommonModule, ProductsRoutingModule, HttpClientModule, PmMaterialModule],
-  providers: [ProductDataService]
+  imports: [
+    CommonModule,
+    ProductsRoutingModule,
+    HttpClientModule,
+    PmMaterialModule,
+    SharedModule,
+  ],
+  providers: [ProductDataService],
 })
 export class ProductsModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from "@angular/core";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
+import { AddToCartComponent } from "./cart/add-to-cart/add-to-cart.component";
 import { CartItemsCountComponent } from "./cart/cart-items-count/cart-items-count.component";
 import { PmMaterialModule } from "./material-module";
 import { SharedRoutingModule } from "./shared-routing.module";
 
-
 @NgModule({
-  declarations: [CartItemsCountComponent],
+  declarations: [CartItemsCountComponent, AddToCartComponent],
   imports: [CommonModule, SharedRoutingModule, RouterModule, PmMaterialModule],
   exports: [
     PmMaterialModule,
@@ -18,6 +18,7 @@ import { SharedRoutingModule } from "./shared-routing.module";
     RouterModule,
     FlexLayoutModule,
     CartItemsCountComponent,
+    AddToCartComponent,
   ],
 })
 export class SharedModule {}
