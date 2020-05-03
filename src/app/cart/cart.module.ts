642e65836754d8bdd2a940dded778de183155eba
Rupesh Kumar Tiwari <roopkt@gmail.com>
2020-05-03 12:03:47 -0400

    showing empty cart message

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -1,14 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { CartRoutingModule } from './cart-routing.module';
-import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { SharedModule } from "@shared/shared.module";
+import { CartRoutingModule } from "./cart-routing.module";
+import { ShoppingCartComponent } from "./shopping-cart/shopping-cart.component";
 
 @NgModule({
   declarations: [ShoppingCartComponent],
-  imports: [
-    CommonModule,
-    CartRoutingModule
-  ]
+  imports: [CommonModule, CartRoutingModule, SharedModule],
 })
-export class CartModule { }
+export class CartModule {}
--- src/app/cart/shopping-cart/shopping-cart.component.ts
+++ src/app/cart/shopping-cart/shopping-cart.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { getCartItemsCount } from "@core/cart/cart-selector";
+import { CartStore } from "@core/cart/cart-store";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'pm-shopping-cart',
-  templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.scss']
+  selector: "pm-shopping-cart",
+  templateUrl: "./shopping-cart.component.html",
+  styleUrls: ["./shopping-cart.component.scss"],
 })
 export class ShoppingCartComponent implements OnInit {
-
-  constructor() { }
+  cartItemsCount: Observable<number>;
+  constructor(private cartStore: CartStore) {}
 
   ngOnInit() {
+    this.cartItemsCount = this.cartStore.select(getCartItemsCount);
   }
-
 }
