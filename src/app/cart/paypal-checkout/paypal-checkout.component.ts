fb4bd52594fb5009493aa80bfcc15c8af8a6fa24
Rupesh Kumar Tiwari <roopkt@gmail.com>
2020-06-16 09:00:38 -0400

    showing checkout button

---
+++ src/app/cart/paypal-checkout/paypal-checkout.component.ts
@@ -0,0 +1,89 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit,
+} from "@angular/core";
+import { Router } from "@angular/router";
+import { getOrderTotal } from "@core/cart/cart-selector";
+import { CartStore } from "@core/cart/cart-store";
+import { CartService } from "@core/cart/cart.service";
+import { LogService } from "@core/log.service";
+import { Subscription } from "rxjs";
+
+declare let paypal: any;
+
+@Component({
+  selector: "pm-paypal-checkout",
+  templateUrl: "./paypal-checkout.component.html",
+  styleUrls: ["./paypal-checkout.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PaypalCheckoutComponent implements OnInit, OnDestroy {
+  orderTotalSubscription: Subscription;
+  orderTotalToCharge: number;
+  constructor(
+    private cartService: CartService,
+    private router: Router,
+    private cartStore: CartStore,
+    private logService: LogService
+  ) {}
+
+  ngOnInit() {
+    // get order total.
+
+    this.orderTotalSubscription = this.cartStore
+      .select(getOrderTotal)
+      .subscribe((orderTotal: number) => {
+        this.logService.log(`Get order total to charge: ${orderTotal}`);
+        this.orderTotalToCharge = orderTotal;
+      });
+
+    // render paypal button pass paypal configuration.
+    paypal.Button.render(this.paypalConfig, "#paypal-button-container");
+  }
+
+  ngOnDestroy() {
+    if (this.orderTotalSubscription) {
+      this.orderTotalSubscription.unsubscribe();
+    }
+  }
+
+  get paypalConfig() {
+    return {
+      style: { size: "responsive" },
+      env: "sandbox",
+      client: { sandbox: "AeYE9X39K74-yVLawilg-gzZzaVPhomdtWXoco94yoqAqeLP5MD93efN_LDVb4vG91eqFTSaJjFP4AhE" },
+      commit: true,
+      payment: (data, actions) => {
+        return actions.payment.create({
+          payment: {
+            transactions: [
+              {
+                amount: {
+                  total: this.orderTotalToCharge,
+                  currency: "USD", // in production you may want to support other currency.
+                },
+              },
+            ],
+          },
+        });
+      },
+      onAuthorize: (data, actions) => {
+        // clear cart
+        // navigate to shopping page
+        return actions.payment.execute().then((payment) => {
+          this.logService.log(`The payment is successful`, payment);
+          this.cartService.clearCart();
+          this.router.navigate(["products"]);
+        });
+      },
+      onCancel: (data) => {
+        this.logService.log(`The payment is cancelled`, data);
+      },
+      onError: (error) => {
+        this.logService.log(`Payment Error`, error);
+      },
+    };
+  }
+}
