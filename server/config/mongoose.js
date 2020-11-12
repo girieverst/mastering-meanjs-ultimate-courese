a8182e1d50dffce484a6fd76d02bd1a8126438a1
Rupesh Kumar Tiwari <roopkt@gmail.com>
2020-11-12 09:07:31 -0500

    fixing server debuging and upgrade packages

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,24 +1,24 @@
-const mongoose = require('mongoose');
-const util = require('util');
-const debug = require('debug')('express-mongoose-es6-rest-api:index');
-const config = require('../config/config');
+const mongoose = require("mongoose");
+const util = require("util");
+const debug = require("debug")("express-mongoose-es6-rest-api:index");
+const config = require("../config/config");
 
 const mongoUri = config.mongo.uri;
 
-mongoose.connect(mongoUri, { keepAlive: 1, useNewUrlParser: true });
+mongoose.connect(mongoUri, { keepAlive: 1, useNewUrlParser: true, useUnifiedTopology:true });
 
 const db = mongoose.connection;
 
-db.once('open', () => {
+db.once("open", () => {
   console.log(`connected to the database: ${mongoUri}`);
 });
 
-db.on('error', () => {
+db.on("error", () => {
   throw new Error(`unable to connect to the database : ${mongoUri}`);
 });
 
 if (config.mongo.isDebug) {
-  mongoose.set('debug', (collectionName, method, query, doc) => {
+  mongoose.set("debug", (collectionName, method, query, doc) => {
     debug(`${collectionName}.${method}`, util.inspect(query, false, 20), doc);
   });
 }
